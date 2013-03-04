bd7415f49058f82ff9386ae3715a016c52e0ee30
Sergio Machado <sergio@umbc.edu>
2013-03-04 11:39:15 -0500

    SSM:cleaned up sarta routines

---
+++ clouds/sarta/cloud_mean_press.m
@@ -0,0 +1,54 @@
+function aa = cloud_mean_press(aaIN,xcumsum,icecld,watercld,plevs,ii)
+
+aa = aaIN;
+
+icecldX   = icecld;
+watercldX = watercld;
+
+%% these are basically the pdfs
+icecldX = icecldX/nansum(icecldX);
+watercldX = watercldX/nansum(watercldX);
+
+%% these are the weights; 
+icecldXW = ones(size(icecldX));%% unit weight
+watercldXW = ones(size(watercldX));%% unit weight
+icecldYW = ones(size(icecldX));%% unit weight
+watercldYW = ones(size(watercldX));%% unit weight
+
+%% better to weight more at Top rather than Bottom - look at blahXW
+%% look at cumsum from top to bottom               - look at blahYW
+if plevs(1) < plevs(10)
+  icecldXW = flipud(cumsum(flipud(icecldX)));    
+  watercldXW = flipud(cumsum(flipud(watercldX)));    
+
+  icecldYW = cumsum((icecldX));    
+  watercldYW = cumsum((watercldX));    
+
+else
+  icecldXW = cumsum((icecldX));    
+  watercldXW = cumsum((watercldX));    
+
+  icecldYW = flipud(cumsum(flipud(icecldX)));    
+  watercldYW = flipud(cumsum(flipud(watercldX)));    
+end
+
+%% this is mean cld pressure (a^0 = so no weighting!!!)
+aa.icecldX(ii) = nansum((plevs.*(icecldXW.^0)).*icecldX);
+aa.watercldX(ii) = nansum((plevs.*(watercldXW.^0)).*watercldX);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% this is where we already have xcumsum of column total
+bonk = find(icecldYW > xcumsum,1);
+if length(bonk) > 0 & xcumsum > 0
+  aa.icecldY(ii) = plevs(bonk);
+else
+  aa.icecldY(ii) = 1200;
+end
+bonk = find(watercldYW > xcumsum,1);
+if length(bonk) > 0  & xcumsum > 0
+  aa.watercldY(ii) = plevs(bonk);
+else
+  aa.watercldY(ii) = 1200;  
+end
+
---
+++ clouds/sarta/compare_slabVSprofile.m
@@ -0,0 +1,61 @@
+function y = compare_slabVSprofile(p,ii)
+%% this script assume you have profile "p" and allows you to compare slab vs cloud profile for water and ice
+
+% ii = input('Enter profile number : ');
+
+
+nlev   = p.nlevs(ii);
+p_prof = p.plevs(1:nlev,ii);
+ice_prof   = p.ciwc(1:nlev,ii);  % in g/g
+water_prof = p.clwc(1:nlev,ii);  % in g/g
+cc_prof    = p.cc(1:nlev,ii);    % 0 < cc < 1
+
+type1 = p.ctype(ii);
+type2 = p.ctype2(ii);
+
+if type1 == 101
+  color1 = 'c';
+elseif type1 == 201
+  color1 = 'm';
+end
+
+if type2 == 101
+  color2 = 'c';
+elseif type2 == 201
+  color2 = 'm';
+end
+
+norm = 1e6;
+
+xice   = ice_prof;   xice   = cumsum(xice);
+xwater = water_prof; xwater = cumsum(xwater);
+plot(water_prof,p_prof,'b',xwater,p_prof,'b--',...
+         ice_prof,p_prof,'r',xice,p_prof,'r--','linewidth',2);
+
+if p.cngwat(ii) > 0
+  line([0 p.cngwat(ii)/norm],[p.cprtop(ii) p.cprtop(ii)],'color',color1)
+  line([0 p.cngwat(ii)/norm],[p.cprbot(ii) p.cprbot(ii)],'color',color1)
+  line([p.cngwat(ii)/norm p.cngwat(ii)/norm],[p.cprbot(ii) p.cprtop(ii)],'color',color1)
+  shade2(gcf,0,p.cprbot(ii),p.cngwat(ii)/norm,p.cprtop(ii)-p.cprbot(ii),color1,0.25);
+  line([0 p.cngwat(ii)/norm],[p.icecldX(ii) p.icecldX(ii)])
+end
+
+if p.cngwat2(ii) > 0
+  line([0 p.cngwat2(ii)/norm],[p.cprtop2(ii) p.cprtop2(ii)],'color',color2)
+  line([0 p.cngwat2(ii)/norm],[p.cprbot2(ii) p.cprbot2(ii)],'color',color2)
+  line([p.cngwat2(ii)/norm p.cngwat2(ii)/norm],[p.cprbot2(ii) p.cprtop2(ii)],'color',color2)
+  shade2(gcf,0,p.cprbot2(ii),p.cngwat2(ii)/norm,p.cprtop2(ii)-p.cprbot2(ii),color2,0.25);
+  line([0 p.cngwat2(ii)/norm],[p.watercldX(ii) p.watercldX(ii)])
+end
+
+set(gca,'ydir','reverse');
+%hl = legend('water','ice','waterslab','iceslab');
+hl = legend('water','ice');
+set(hl,'fontsize',12)
+
+xlabel('mixing ratio g/g','fontsize',10); ylabel('pressure (mb)','fontsize',10)
+set(gca,'fontsize',10)
+
+hx = axis;
+axis([hx(1) hx(2) 100 1000]);
+
