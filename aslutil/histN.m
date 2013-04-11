53b3ccaab409e8231dfed3b3efea468b52e9e069
Paul Schou <schou@umbc.edu>
2013-04-11 15:02:56 -0400

    Adding case where the edge adds an N+1 bin and supporting debug output for lost data points.

--- aslutil/histN.m
+++ aslutil/histN.m
@@ -37,15 +37,20 @@ end
 % predeclare the bin set matrix
 %histmat = zeros(outsize);
 bins = zeros(numel(varargin{1}),length(outsize));
+bin_zaps = zeros(1,length(outsize));
 
 % the remaining part of this code is taken out of gstats and simplified
 for i = 1:nargin/2
   [x bins(:,i)] = histc(varargin{i}(:),varargin{i+nargin/2});
+  % remove any infinities or reasons for the N+1 bin
+  bins(bins(:,i) >= length(varargin{i+nargin/2}),i) = 0;
+  % keep track of how many points did not make a bin
+  bin_zaps(i) = sum(bins(:,i) == 0);
 end
 
 % remove zeros
 if any(bins(:) == 0)
-  disp(['Warning: ' num2str(sum(bins(:)==0)) ' of ' num2str(size(bins,1)) ' datapoints not counted in the selected bin sizes'])
+  disp(['Warning: ' num2str(sum(bins(:)==0)) ' of ' num2str(size(bins,1)) ' datapoints not counted in the selected bin sizes.  Bin drops: {' num2str(bin_zaps,'%g ') '}'])
   bins = bins(~any(bins == 0,2),:);
 end
 
