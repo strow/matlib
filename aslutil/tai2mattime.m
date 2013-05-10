805be472a37dba1ff465f1c6db60f5538a6c6481
Breno Imbiriba <breno.imbiriba@gmail.com>
2013-05-10 16:08:47 -0400

    Improved on mattime2tai and tai2mattime to work with variable startup year

--- aslutil/tai2mattime.m
+++ aslutil/tai2mattime.m
@@ -1,7 +1,19 @@
-function dateout = tai2mattime(datein)
-%function dateout = tai2mattime(datein)
-%  Airs to matlab time conversion
+function dateout = tai2mattime(datein, y0)
+%function dateout = tai2mattime(datein, y0)
+%  TAI to mattime conversion
+%
+%  datein - TAI time from year0 (seconds past y0/1/1 0:0:0)
+%  year0  - start year (if not present, will use 1993)
+%           Use AIRS - 1993, IASI/CrIS - 2000
+%  
 % See also:  iasi2mattime, mattime2tai, mattime2iasi
 
+% Paul Schou/Breno Imbiriba - 2013.05.10
 
-dateout = datenum(1993,1,1,0,0,double(datein));
+  if(nargin==1)
+    y0=1993;
+  end
+
+  dateout = datenum(y0,1,1,0,0,double(datein));
+
+end
