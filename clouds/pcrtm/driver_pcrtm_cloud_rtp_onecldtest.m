e29c6c85656f46d2ca4867fc56d031128f311c8e
Sergio Machado <sergio@umbc.edu>
2015-08-02 09:41:10 -0400

    SSM : changed default sarta clear code from PGE to /asl/packages/sartaV108/BinV201/sarta_apr08_m140_wcon_nte, and default cloud code to /home/sergio/SARTA_CLOUDY/BinV201/sarta_apr08_m140_iceGHMbaum_waterdrop_desertdust_slabcloud_hg3

--- clouds/pcrtm/driver_pcrtm_cloud_rtp_onecldtest.m
+++ clouds/pcrtm/driver_pcrtm_cloud_rtp_onecldtest.m
@@ -82,10 +82,12 @@ elseif nargin == 5
   end   
   if ~isfield(run_sarta,'sartaclear_code')
     run_sarta.sartaclear_code = '/asl/packages/sartaV108_PGEv6/Bin/sarta_airs_PGEv6_postNov2003';
+    run_sarta.sartaclear_code = '/asl/packages/sartaV108/BinV201/sarta_apr08_m140_wcon_nte';
   end
   if ~isfield(run_sarta,'sartacloud_code')
     %run_sarta.sartacloud_code = '/asl/packages/sartaV108/Bin/sarta_apr08_m140_iceaggr_waterdrop_desertdust_slabcloud_hg3_wcon_nte';
     run_sarta.sartacloud_code = '/asl/packages/sartaV108/BinV201/sarta_apr08_m140_iceaggr_waterdrop_desertdust_slabcloud_hg3_wcon_nte';
+    run_sarta.sartacloud_code = '/home/sergio/SARTA_CLOUDY/BinV201/sarta_apr08_m140_iceGHMbaum_waterdrop_desertdust_slabcloud_hg3';    
   end
 end
 
